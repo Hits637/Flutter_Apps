08607e6d3c850492520d8c799638faf78ec1886d
hits637 <hiteshpatil.skn.entc@gmail.com>
2024-10-04 01:10:46 +0530

    Scroll

---
+++ Assignments/column_with_scroll/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Column With Scroll",
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              )),
+          centerTitle: true,
+          backgroundColor: Colors.cyan,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Image.network(
+                  "https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg"),
+              const SizedBox(
+                height: 30,
+              ),
+              Image.network(
+                  "https://wallpapers.com/images/featured/anime-scenery-ahd1eqpvwq8aoofp.jpg"),
+              const SizedBox(
+                height: 30,
+              ),
+              Image.network(
+                  "https://cdn.oneesports.gg/cdn-data/2023/01/Anime_DemonSlayer_AllHashira_2.jpg"),
+              const SizedBox(
+                height: 30,
+              ),
+              Image.network(
+                  "https://cdn.oneesports.gg/cdn-data/2024/01/Anime_DemonSlayer_Giyu.jpg"),
+              const SizedBox(
+                height: 30,
+              ),
+              Image.network(
+                  "https://assets-prd.ignimgs.com/2022/08/17/top25animecharacters-blogroll-1660777571580.jpg"),
+              const SizedBox(
+                height: 30,
+              ),
+              Image.network(
+                  "https://www.animationmagazine.net/wordpress/wp-content/uploads/Jujutsu-Kaisen-6.jpg"),
+              const SizedBox(
+                height: 30,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
